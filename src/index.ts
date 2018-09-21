2216b9ced92b419271cab71951f97a6a6cf8e299
Ravi Shankar <wafflespeanut@gmail.com>
2018-09-21 16:45:01 +0530

    Load script in all pages and toggle 'active' class in buttons

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+let elements = document.querySelectorAll("button.dropdown-button");
+elements.forEach(button => {
+    console.log("Adding event listener to", button);
+    button.addEventListener("click", () => {
+        if (button.classList.contains("active")) {
+            button.classList.remove("active");
+        } else {
+            button.classList.add("active");
+        }
+    });
+});
